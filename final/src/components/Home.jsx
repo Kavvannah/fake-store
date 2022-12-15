d56eb8286cf2ac6c3014a453b88a1c9eecd42828
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2022-12-15 10:37:34 -0800

    :heavy_plus_sign:fetched products

--- final/src/components/Home.jsx
+++ final/src/components/Home.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+import Products from './Products';
 
 const Home = () => {
   return (
     <div className="parrallax">
-      <div class="card text-bg-dark border-0">
+      <div className="card text-bg-dark border-0">
         <img
           src="/assets/laura-chouette-JKOD4x9A79Y-unsplash.jpg"
-          class="card-img"
+          className="card-img"
           alt="Background"
           height="500px"
         />
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
-            <h5 class="card-title display-3 fw-bolder">SALE BELL RINGS</h5>
-            <p class="card-text lead fs-2">
+            <h5 className="card-title display-3 fw-bolder">SALE BELL RINGS</h5>
+            <p className="card-text lead fs-2">
               Ali Beauty and New in this week
             </p>
-            <p class="card-text lead fs-2">
+            <p className="card-text lead fs-2">
               Up to 70% off selected items
             </p>
           </div>
         </div>
       </div>
+      
+    {/* products */}
+    <Products/>
     </div>
   );
 };
